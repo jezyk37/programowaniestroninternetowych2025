35dec1d75a197d04aef13c6e8a39091e9b260642
xdd <jan.grzegorski3@gmial.com>
2025-02-06 12:58:58 +0100

    lekcja 30 stycznia

---
+++ 30.01.2025/zadanie4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $wiek = 14;
+
+       if($wiek > 10){
+        echo "zmienna wiek jest wieksza od 10";
+       }elseif($wiek==10){
+        echo "zmienna wiek jest rowna 10";
+       }else{
+        echo "zmienna wiek jest mniejsza od 10";
+       }
+    ?>
+</body>
+</html>
---
+++ 30.01.2025/zadanie9.php
@@ -0,0 +1,18 @@
+<?php
+
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>To jest nagłowek</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatum praesentium minus ad? Voluptate amet, dolorum dolorem labore quo laboriosam aliquam ullam eveniet cum. Totam at culpa alias fuga corporis.</p>
+</body>
+</html>
+';
+
+?>
